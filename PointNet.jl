cc4f1e64845bcf2015dd8c9c96560cc8bbc0574c
nirmal <nirmalps@iitk.ac.in>
2020-03-25 15:53:46 +0530

    Model up and running

--- PointNet.jl
+++ PointNet.jl
@@ -1,6 +1,5 @@
-using Flux, Metalhead, Statistics, LinearAlgebra, Zygote
+using Flux, Metalhead, Statistics, LinearAlgebra, Zygote, Random
 using Flux: onehotbatch, onecold, onehot, crossentropy, throttle, NNlib
-using Metalhead: trainimgs
 using Images: channelview
 using Statistics: mean
 using Base.Iterators: partition
@@ -15,43 +14,77 @@ using Base.Iterators: partition
 STNkd(k::Int) = Chain(
     x -> reshape(x, :, 1, k, size(x, 3)),
     Conv((1, 1), k=>64, relu),
-    BatchNorm(64),
     Conv((1, 1), 64=>128, relu),
-    BatchNorm(128),
     Conv((1, 1), 128=>1024, relu),
-    BatchNorm(1024),
     x -> reshape(maximum(reshape(x, :, 1024, size(x,4)), dims=1), :, size(x, 4)),
     Dense(1024, 512, relu),
-    BatchNorm(512),
     Dense(512, 256, relu),
-    BatchNorm(256),
     Dense(256,k*k),
     x -> reshape(x, k, k, size(x, 2)),
-    x -> x .+ reshape(Matrix{Float64}(I, k, k), k, k, 1)
+    x -> x .+ reshape(Matrix{Float64}(I, k, k), k, k, 1),
+    x -> PermutedDimsArray(x, (2,1,3)),
 )
 
 PointNet(k::Int) = Chain(
-    x -> batched_mul(x, batched_transpose(stn(x))),
+    x -> batched_mul(x, stn(x)),
     x -> reshape(x, :, 1, size(x, 2), size(x, 3)),
     Conv((1,1), 3=>64),
-    BatchNorm(64),
     x -> reshape(x, :, size(x, 3), size(x, 4)),
-    x -> batched_mul(x, batched_transpose(fstn(x))),
+    x -> batched_mul(x, fstn(x)),
     x -> reshape(x, :, 1, size(x, 2), size(x, 3)),
     Conv((1,1), 64=>128, relu),
-    BatchNorm(128),
     Conv((1,1), 128=>1024),
-    BatchNorm(1024),
     x -> reshape(x, :, size(x, 3), size(x, 4)),
     x -> reshape(maximum(x, dims=1), 1024, size(x, 3)),
     Dense(1024, 512, relu),
-    BatchNorm(512),
     Dense(512, 256, relu),
     Dropout(0.4),
-    BatchNorm(256),
     Dense(256, k, relu),
-    softmax
+    softmax,
 )
+# issue with using Batchnorm reducedims_init() 
+# STNkd_bn(k::Int) = Chain(
+#     x -> reshape(x, :, 1, k, size(x, 3)),
+#     Conv((1, 1), k=>64, relu),
+#     BatchNorm(64),
+#     Conv((1, 1), 64=>128, relu),
+#     BatchNorm(128),
+#     Conv((1, 1), 128=>1024, relu),
+#     BatchNorm(1024),
+#     x -> reshape(maximum(reshape(x, :, 1024, size(x,4)), dims=1), :, size(x, 4)),
+#     Dense(1024, 512, relu),
+#     BatchNorm(512),
+#     Dense(512, 256, relu),
+#     BatchNorm(256),
+#     Dense(256,k*k),
+#     x -> reshape(x, k, k, size(x, 2)),
+#     x -> x .+ reshape(Matrix{Float64}(I, k, k), k, k, 1),
+#     x -> PermutedDimsArray(x, (2,1,3)),
+# )
+
+# PointNet_bn(k::Int) = Chain(
+#     x -> batched_mul(x, stn(x)),
+#     x -> reshape(x, :, 1, size(x, 2), size(x, 3)),
+#     Conv((1,1), 3=>64),
+#     BatchNorm(64),
+#     x -> reshape(x, :, size(x, 3), size(x, 4)),
+#     x -> batched_mul(x, fstn(x)),
+#     x -> reshape(x, :, 1, size(x, 2), size(x, 3)),
+#     Conv((1,1), 64=>128, relu),
+#     BatchNorm(128),
+#     Conv((1,1), 128=>1024),
+#     BatchNorm(1024),
+#     x -> reshape(x, :, size(x, 3), size(x, 4)),
+#     x -> reshape(maximum(x, dims=1), 1024, size(x, 3)),
+#     Dense(1024, 512, relu),
+#     BatchNorm(512),
+#     Dense(512, 256, relu),
+#     Dropout(0.4),
+#     BatchNorm(256),
+#     Dense(256, k, relu),
+#     softmax,
+# )
+
 
 function pcNormalize(pc)
     centroid = mean((x->x), pc, dims = 1)
@@ -101,6 +134,11 @@ split_ = "test"
 shape_names[split_] = [join(split(shapeids[split_][i], "_")[1:end-1], "_") for i in 1:length(shapeids[split_])]
 datapath[split_] = [(shape_names[split_][i], joinpath(root, shape_names[split_][i], (shapeids[split_][i])*".txt")) for i in 1:length(shapeids[split_])]
 
+#shuffling trainset Point
+rng = MersenneTwister(1234);
+shuffle!(rng, datapath["train"])
+
+
 # input: width*height*channel*minibatch
 
 # Fetching the train and validation data and getting them into proper shape
@@ -131,8 +169,10 @@ opt = ADAM()
 
 # Starting to train models
 
-@show("Training Start")
+print("Training Start")
 Flux.train!(loss, params(m), train, opt, cb = evalcb)
+print("Training Finished")
+
 
 # Accuracy of valset of ModelNet dataset
 @show(accuracy(valX, valY))
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PointNet
 Implementation of PointNet classification in Flux machine learning library written in Julia.
+
+For downloading and processing the modelnet40 dataset.
+```bash
+$ URL=https://shapenet.cs.stanford.edu/media/modelnet40_normal_resampled.zip
+$ ZIP_FILE=./data/modelnet40.zip
+$ mkdir -p ./data/
+$ wget -N $URL -O $ZIP_FILE
+$ unzip $ZIP_FILE -d ./data/
+$ rm $ZIP_FILE
+```
+    
+For running the model
+
+```bash
+$ julia --project="." PointNet.jl
+```
